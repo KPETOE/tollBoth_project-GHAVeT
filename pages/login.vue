4fd1d0d6dee55fd0432f4f32fb0014105af640a5
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2024-02-16 00:51:54 +0000

    Update login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -39,7 +39,7 @@
                     </v-col>
                 </v-row>
                 <br>
-                <p class="text-center">Don't have an account? <nuxt-link to="/signup">Signup here</nuxt-link></p>
+                <p class="text-center">Don't have an account? <nuxt-link style="text-decoration: none;" class="text-white" to="/signup">Signup here</nuxt-link></p>
                 <v-row align="center" justify="center">
                     <v-col cols="12" sm="6">
                         <v-btn type="submit">Submit</v-btn>
