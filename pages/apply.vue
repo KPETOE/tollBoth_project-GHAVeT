4c35d8ceb6f9a6c897bbcb714fbb004d210170c6
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2024-03-02 18:07:26 +0000

    Update apply.vue

--- pages/apply.vue
+++ pages/apply.vue
@@ -242,7 +242,7 @@
                         </v-col>
 
                         <v-col align="right" cols="" lg="6" md="6" sm="6">
-                            <v-btn @click="handleSubmit" type="submit" prepend-icon="mdi-check-circle" class="B-clear">
+                            <v-btn @click="setTimeout(handleSubmit(), 6000)" type="submit" prepend-icon="mdi-check-circle" class="B-clear">
                                 <template v-slot:prepend>
                                     <v-icon color="success"></v-icon>
                                 </template>
@@ -310,40 +310,29 @@ const checkbox = ref(false);
 
 //submit function
 const submitApplication = async () => {
-    // Handle form submission logic
-    // You can perform additional validation or submit data to the server
-    // if (checked == ! true) {
-    //     alert('You have not agreed!')
-    // }
     // if (checkbox == !true) {
     //     alert('Check the Box first! :)')
     // }
 
     let x = 0;
-    // const y = x++;
+   
     try {
         alert.value = true
-        // if (checkbox == true) {
-        //     console.log('Null')
-        // } else {
+        
         const { data, error } = await client.from('application').insert([
             {
                 GHAVeTag: applyForm.value.ghCard + '-' + x++,
                 Licenced_plate: applyForm.value.license,
-                // newtag: ownForm.value.newTag,
                 Gh_card: applyForm.value.ghCard,
                 residence: applyForm.value.residence,
                 veh_type: applyForm.value.veh_type,
                 Insurance: applyForm.value.insurance,
                 Pickup_loc: applyForm.value.pickup_loc,
-                // user_id: user.value.id
             }
         ]).select();
-        alert('Submitted Successfully!')
         const dialog = true
-
-        // }
-
+        
+        
 
     } catch (error) {
         console.log(error)
