1987d939f1fb01d791b54aff929fc7b512add422
KPETOE <Kuseamorichardjampana@gmail.com>
2024-06-27 10:39:43 -1200

    update login

--- pages/login.vue
+++ pages/login.vue
@@ -1,58 +1,61 @@
 <template>
     <div class="body">
-        <h1 class="text-center">Welcome</h1>
-        <h2 class="text-center">Glad to having you</h2>
+        <div  style="width: 40%;"  class=" CARD" justify="center">
+            <h3 class="text-center">Welcome</h3>
+            <h5 class="text-center">Glad to having you</h5>
 
 
-        <v-row class="text-center" align="center" justify="center">
-            <v-col cols="12" sm="6">
-                <v-avatar size="400">
-                    <v-img src="/img/LoginLogo.png" contain fill-height></v-img>
-                </v-avatar>
+            <v-col class="text-center custom-offset" align="center" justify="center">
+                <v-row cols="12" sm="6" >
+                    <v-avatar size="400" >
+                        <v-img src="/img/LoginLogo.png" contain fill-height></v-img>
+                    </v-avatar>
+                </v-row>
             </v-col>
-        </v-row>
 
 
-        <v-container fluid style="height: ">
-            <v-form @submit.prevent="signIn" style="text-align: center;">
-                <v-row align="center" justify="center">
+            <v-container fluid style="height: " class="">
+                <v-form @submit.prevent="signIn" style="text-align: center;">
+                    <v-row align="center" justify="center">
 
-                    <v-col cols="12" sm="6" class="label-left">
-                        <v-label class="text-white " style="font-size: 1.2em; font-weight: bold;"> Username /
-                            Email</v-label>
-                        <v-text-field class="text-field text-center" type="name" v-model="loginForm.userName"
-                            placeholder="User Name/Email" variant="flat"></v-text-field>
-                    </v-col>
-                </v-row>
-                <br>
-                <v-row align="center" justify="center">
+                        <v-col cols="12" sm="6" class="label-left">
+                            <v-label class="text-white " style="font-size: 1.2em; font-weight: bold;"> Username /
+                                Email</v-label>
+                            <v-text-field class="text-field text-center" type="name" v-model="loginForm.userName"
+                                placeholder="User Name/Email" variant="flat"></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <br>
+                    <v-row align="center" justify="center">
 
 
-                    <v-col cols="12" sm="6" class="label-left">
-                        <v-label class="text-white  " style="font-size: 1.2em; font-weight: bold; ">
-                            Password</v-label>
-                        <v-text-field class="text-field text-center" v-model="loginForm.password"
-                            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                            :type="showPassword ? 'text' : 'password'" @click:append="showPassword = !showPassword"
-                            variant="flat" placeholder="Password">
-                        </v-text-field>
-                    </v-col>
+                        <v-col cols="12" sm="6" class="label-left">
+                            <v-label class="text-white  " style="font-size: 1.2em; font-weight: bold; ">
+                                Password</v-label>
+                            <v-text-field class="text-field text-center" v-model="loginForm.password"
+                                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                                :type="showPassword ? 'text' : 'password'" @click:append="showPassword = !showPassword"
+                                variant="flat" placeholder="Password">
+                            </v-text-field>
+                        </v-col>
 
-                </v-row>
+                    </v-row>
 
-                <br>
-                <p class="text-center">Don't have an account? <nuxt-link style="text-decoration: none;" class="text-white"
-                        to="/signup">Signup here</nuxt-link></p><br>
-                <p class="text-center">Forgot Password? <nuxt-link style="text-decoration: none;" class="text-white"
-                        to="/forgot">Click here</nuxt-link></p>
-                <v-row align="center" justify="center">
-                    <v-col cols="12" sm="6">
-                        <v-btn type="submit">Submit</v-btn>
-                    </v-col>
-                </v-row>
-            </v-form>
-            <br> <br>
-        </v-container>
+                    <br>
+                    <p class="text-center">Don't have an account? <nuxt-link style="text-decoration: none;"
+                            class="text-white" to="/signup">Signup here</nuxt-link></p><br>
+                    <p class="text-center">Forgot Password? <nuxt-link style="text-decoration: none;" class="text-white"
+                            to="/forgot">Click here</nuxt-link></p>
+                    <v-row align="center" justify="center">
+                        <v-col cols="12" sm="6">
+                            <v-btn type="submit">Submit</v-btn>
+                        </v-col>
+                    </v-row>
+                </v-form>
+                <br> <br>
+            </v-container>
+
+        </div>
     </div>
 </template>
 
@@ -133,7 +136,7 @@ const showPassword = ref(false);
 
 <style>
 .text-field {
-    width: 35em;
+    width: 25em;
     height: 3.5em;
 
     background: linear-gradient(to right, rgba(252, 252, 252, 0.331), rgba(122, 200, 230, 0.486), #31759ab4, #4f819c73) !important;
@@ -155,4 +158,19 @@ const showPassword = ref(false);
     flex-direction: column;
     align-items: flex-start;
 }
+
+
+.CARD {
+  padding: 20px;
+  box-shadow: 0 0 35px rgba(81, 151, 151, 0.562);
+  border-radius: 10px;
+  max-width: 800px;
+  margin: auto;
+  height: 60%;
+
+ 
+}
+
+.custom-offset {
+    margin-left: 70px; }
 </style>
